e81f4b88a1c49a5fb97b66851b0fc71e877941b3
Jisung you <gs47298272@gmail.com>
2025-11-26 01:32:27 +0900

    bassup

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
-import numpy as np
-from src.stft import stft
-from src.istft import istft
+import os
+from pydub import AudioSegment
 
-FILENAME = 'YUDABINBAND_-_(mp3.pm).mp3'
-CUTOFF_HZ = 200
-N_FFT = 2048
-y, sr = librosa.load(FILENAME, sr=44100)
+base_path = "separated\htdemucs\YUDABINBAND_-_(mp3.pm)" 
 
-mag, phase = stft(y, sr, n_fft=N_FFT)
 
-cutoff_bin = int(CUTOFF_HZ * N_FFT / sr)
+try:
+    bass = AudioSegment.from_wav(os.path.join(base_path, "bass.wav"))
+    
+    print("트랙 로드 완료!")
 
-mag_bass = mag.copy()
-mag_bass[cutoff_bin:, :] = 0 
-plt.figure(figsize=(12, 10))
-plt.subplot(2, 1, 1)
-mag_db = librosa.amplitude_to_db(mag, ref=np.max)
-librosa.display.specshow(mag_db, sr=sr, x_axis='time', y_axis='log')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Original')
+except FileNotFoundError:
+    print("실패")
+    exit()
 
-# bass
-plt.subplot(2, 1, 2)
-mag_bass_db = librosa.amplitude_to_db(mag_bass, ref=np.max)
-librosa.display.specshow(mag_bass_db, sr=sr, x_axis='time', y_axis='log')
-plt.colorbar(format='%+2.0f dB')
-plt.title(f'Bass Only (Under {CUTOFF_HZ}Hz)')
+print("2. 베이스 이펙트 적용 중...")
+# 베이스 사운드 상승
+processed_bass = bass + 6 
 
-plt.tight_layout()
-plt.show()
+
+output_filename = "add_sound_BassBoost.mp3"
+print(f"4. {output_filename} 파일로 저장 중...")
+
+processed_bass.export(output_filename, format="mp3")
+
+print("done!")
