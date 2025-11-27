7e67c87c4a8134bc883a26918cd2efe6c8312024
Sangbong Kim <ksbong06@gmail.com>
2025-11-27 20:17:28 +0900

    Edit: guitar/effects.py to setup initial skeleton for effect classes

---
+++ hystemfx/guitar/effects.py
@@ -0,0 +1,31 @@
+from pedalboard import (
+    PedalBoard, Compressor, Distortion,
+    Chorus, Delay, Reverb, Gain, NoiseGate
+)
+
+class EffectBlock():
+    pass
+
+class FX_Compressor():
+    name = "compressor"
+    pass
+
+class FX_Distortion():
+    name = "distortion"
+    pass
+
+class FX_Delay():
+    name = "delay"
+    pass
+
+class FX_Reverb():
+    name = "reverb"
+    pass
+
+class FX_Gain():
+    name = "gain"
+    pass
+
+class FX_NoiseGate():
+    name = "noise"
+    pass
