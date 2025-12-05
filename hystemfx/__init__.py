5a223a094dac29e601da7a9f8fdcbaabca5d4c57
Sangbong Kim <ksbong06@gmail.com>
2025-12-06 00:37:51 +0900

    update dependencies and remove unused source files

--- hystemfx/__init__.py
+++ hystemfx/__init__.py
@@ -1,4 +1,19 @@
-from .pipeline import run_pipeline
+from .pipeline import run_pipeline, process_stem
+
+# Session Pipelines & Convenience Functions
+from .vocal.pipeline import VocalPipeline, process_vocal_from_mix
+from .guitar.pipeline import GuitarPipeline, process_guitar_from_mix
+from .bass.pipeline import BassPipeline, process_bass_from_mix
+from .synth.pipeline import SynthPipeline, process_synth_from_mix
+
+# Separators
+from .vocal.separator import VocalSeparator, separate_vocal
+from .guitar.separator import GuitarSeparator, separate_guitar
+from .bass.separator import BassSeparator, separate_bass
+from .synth.separator import SynthSeparator, separate_synth
+
+# Effects
 from .vocal.effects import VocalRack
-from .synth.effects import SynthEffectsChain
 from .guitar.effects import GuitarEffectsChain
+from .bass.effects import BassRack
+from .synth.effects import SynthEffectsChain
